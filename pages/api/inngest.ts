5c246e76552bc9b6fa7f6dedf14bab66b9854dfa
IgorBialek <igorbialek0@gmail.com>
2022-12-01 23:23:23 +0100

    Inngest Fixes
    - Moved Code

--- pages/api/inngest.ts
+++ pages/api/inngest.ts
@@ -1,5 +1,6 @@
+import axios, { AxiosError } from 'axios';
 import { load } from 'cheerio';
-import { collection, doc, getDocs, setDoc } from 'firebase/firestore';
+import { collection, doc, getDoc, getDocs, setDoc } from 'firebase/firestore';
 import { createScheduledFunction } from 'inngest';
 // ./pages/api/inngest.js
 import { serve } from 'inngest/next';
@@ -10,6 +11,55 @@ import observedAnime from '../../models/observedAnime';
 
 const job = async () => {
   let status = "Success";
+
+  const sendMessage = async (
+    messengerId: string,
+    anime: string,
+    episode: string,
+    episodeNumber: number,
+    url: string
+  ) => {
+    let request_body = {
+      recipient: {
+        id: messengerId,
+      },
+      message: {
+        attachment: {
+          type: "template",
+          payload: {
+            template_type: "button",
+            text: `New episode #${episodeNumber} ${episode} of ${anime} just came out!`,
+            buttons: [
+              {
+                type: "web_url",
+                url: "https://newanime.vercel.app",
+                title: "Open app",
+                webview_height_ratio: "full",
+              },
+              {
+                type: "web_url",
+                url: url,
+                title: "Watch now!",
+                webview_height_ratio: "full",
+              },
+            ],
+          },
+        },
+      },
+    };
+
+    try {
+      await axios.request({
+        url: "https://graph.facebook.com/v2.6/me/messages",
+        method: "post",
+        params: { access_token: process.env.MESSENGER_TOKEN },
+        data: request_body,
+      });
+    } catch (error) {
+      const e = error as AxiosError;
+      console.log(e.response?.data);
+    }
+  };
 
   try {
     const cloudscraper = require("cloudscraper");
@@ -23,6 +73,10 @@ const job = async () => {
         observedAnimeList: observedAnime[];
       };
 
+      let messengerIdSnapshot = await getDoc(
+        doc(firestore, "messengerId", document.id)
+      );
+
       for (const anime of observedAnimeList) {
         let html = await cloudscraper.get(anime.url);
         const $ = load(html);
@@ -31,14 +85,23 @@ const job = async () => {
 
         episodeList.each((i, el) => {
           if (i < episodeList.length - anime.lastSeenEpisode) {
+            let title = $(el).find(".epl-title").text();
+            let number = episodeList.length - i;
+            let url = $(el).attr("href") ?? "";
+
             anime.episodes = [
               ...anime.episodes,
               {
-                title: $(el).find(".epl-title").text(),
-                number: episodeList.length - i,
-                url: $(el).attr("href"),
+                title,
+                number,
+                url,
               } as newEpisode,
             ];
+
+            if (messengerIdSnapshot.exists()) {
+              let { id } = messengerIdSnapshot.data();
+              sendMessage(id, anime.name, title, number, url);
+            }
           }
         });
 
